b27f1180f1d06f37cb3b683526700d0f19d19abf
Louise Eriksson <epost.louise@gmail.com>
2020-06-06 19:11:11 +0200

    storing the cart items in localstorage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
+import { createStore } from 'redux'
 
 import { cart } from 'reducers/cart'
 import { products } from 'reducers/products'
@@ -13,12 +14,41 @@ import { ProductDetails } from 'components/ProductDetails'
 import { Cart } from 'components/Cart'
 import { Footer } from 'components/Footer'
 
+const saveToLocalStorage = (state) => {
+  try {
+    const serializedState = JSON.stringify(state)
+    localStorage.setItem('state', serializedState)
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+const loadFromLocalStorage = () => {
+  try {
+    const serializedState = localStorage.getItem('state')
+    if (serializedState === null) return undefined
+    return JSON.parse(serializedState)
+  } catch (e) {
+    console.log(e)
+    return undefined
+  }
+}
+
 const reducer = combineReducers({
   cart: cart.reducer,
   products: products.reducer
 })
 
-const store = configureStore({ reducer })
+const persistedState = loadFromLocalStorage()
+
+// const store = configureStore({ reducer, persistedState })
+
+const store = createStore(
+  reducer,
+  persistedState
+)
+
+store.subscribe(() => saveToLocalStorage(store.getState()))
 
 export const App = () => {
   return (
