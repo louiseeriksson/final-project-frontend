3d0047da28190d34ee6cebd2a417a504bd22afa7
Louise Eriksson <epost.louise@gmail.com>
2020-06-24 12:48:37 +0200

    cleaned up code

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
 
-
 export const Checkout = () => {
 
   const user = useSelector((state) => state.authentication.user)
   const products = useSelector((store) => store.cart.items)
   const totalPrice = useSelector((store) => (
-    store.cart.items.reduce((total, item) => (total + (item.price * item.quantity)), 0)
-  ))
+    store.cart.items.reduce((total, item) => (total + (item.price * item.quantity)), 0)))
+
   return (
     <div className='page-wrapper'>
+
       <h1 className='page-title'>KASSA</h1>
       <p className='message'>{user.name}, här är en summering av produkterna i din varukorg:</p>
 
       <div>
-        {products.map((product) => (<p className='message'>{product.title}, {product.quantity} st</p>))}
+        {products.map((product) => (
+          <p className='message'>{product.title}, {product.quantity} st</p>))}
       </div>
 
       <p className='message'>SUMMA: {totalPrice} SEK</p>
@@ -49,6 +50,7 @@ export const Checkout = () => {
       </form>
 
       <button className='checkout-button'>GENOMFÖR KÖP</button>
+
     </div>
   )
 }
